b9b72a91b710428b79b714be4b244ed959a84b27
Thyagodevel <thyagoc.desousa@gmail.com>
2022-06-09 00:33:56 -0300

    utilizando ts-node global para nao precisar ficar gerando arquivo js a todo momento na mão

--- Personagem.ts
+++ Personagem.ts
@@ -16,28 +16,28 @@ class Personagem {
       public defesa: number
     ) {
     // define que os atributos DESTA classe deve ser iniciado com esses valores ou valores obrigatórios;
-    this.energia = 50;
-    this.ataque = 50;
-    this.nome = nome;
+    //this.energia = 50;
+    //this.ataque = 50;
+    //this.nome = nome;
   }
 }
 
 //novo objeto estanciado da classe
-let sansa = new Personagem('Sansa Stark');
+let sansa = new Personagem('Sansa Stark', 100, 40, 20, 20);
 
-sansa.ataque = 10;
-sansa.defesa = 20;
-sansa.energia = 50;
-sansa.vida = 100;
+//sansa.ataque = 10;
+//sansa.defesa = 20;
+//sansa.energia = 50;
+//sansa.vida = 100;
 
 console.log('Person: ', sansa);
 
 //novo objeto estanciado da classe
-let jon = new Personagem('Json Snow');
+let jon = new Personagem('Json Snow', 80, 90, 100, 100);
 
-jon.ataque = 80;
-jon.defesa = 70;
-jon.energia = 90;
-jon.vida = 40;
+//jon.ataque = 80;
+//jon.defesa = 70;
+//jon.energia = 90;
+//jon.vida = 40;
 
 console.log('Person: ', jon);
